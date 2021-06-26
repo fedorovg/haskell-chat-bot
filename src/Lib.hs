e63b2f346011567503d39efdc39cb6fcc84bc51a
Gleb Fedorov <fedorgle@fit.cvut.cz>
2021-06-26 20:32:34 +0300

    Created a function for creating an app Environment from configuration

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,7 +5,11 @@ module Lib
 import qualified Logging as Log
 import Bot
 import qualified Data.ByteString.Lazy.Char8 as L8
-
+import Config
+import Network.HTTP.Client
+import API.Weather
+import API.DataAPI
+import API.GenericAPI
 
 -- | Env contains the read-only application environment.
 --   It stores all functions and data used by the application in runtime.
@@ -18,6 +22,27 @@ data Env b
     , envInitialSession :: Session b -- ^ Start state for our bot
     }
 
+
+-- | createEnv is afunction that creates application `Env`ironment from provided Bot implementation and a Config
+createEnv :: Bot b => b -> Config -> Manager -> Env b
+createEnv bot config@(Config _ apiConf) manager =
+    case apiConf of
+        (WeatherApiConfig url tok help) ->
+            Env
+                Log.Info
+                (getUpdates bot manager)
+                (sendMessage bot manager)
+                (getData api manager)
+                (initialSession bot)
+                where api = WeatherAPI tok url help
+        aCfg@GenericApiConfig {} ->
+             Env
+                Log.Info
+                (getUpdates bot manager)
+                (sendMessage bot manager)
+                (getData api manager)
+                (initialSession bot)
+                where api = createGenericApi aCfg
 
 start :: FilePath -> IO ()
 start configPath = undefined 
