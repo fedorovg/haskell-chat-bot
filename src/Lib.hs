9eeb63f0bb6681cd83571bc919ce64e006431e78
Gleb Fedorov <fedorgle@fit.cvut.cz>
2021-06-26 20:06:48 +0300

    Added Environment data type, that will encapsulate the whole app

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,23 @@
 module Lib
-    ( someFunc
+    ( start
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Logging as Log
+import Bot
+import qualified Data.ByteString.Lazy.Char8 as L8
+
+
+-- | Env contains the read-only application environment.
+--   It stores all functions and data used by the application in runtime.
+data Env b
+    = Env
+    { envLog :: !Log.Level -- ^ A log level, so that we can easily disable unnecesasry logging
+    , envGetUpdates :: !(Session b -> IO (Either L8.ByteString (Session b, [(Destination b, L8.ByteString)]))) -- ^ Long polling funciton used to check for new messages
+    , envSendMessage :: Destination b -> L8.ByteString  -> IO L8.ByteString -- ^ Function to respond with a message
+    , envGetData :: !(String -> IO L8.ByteString)  -- ^ Get data, used as a response for the message 
+    , envInitialSession :: Session b -- ^ Start state for our bot
+    }
+
+
+start :: FilePath -> IO ()
+start configPath = undefined 
