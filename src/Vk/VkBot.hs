1a7f37feb2a9ca2609fd3df0b596908b9462e0c8
Gleb Fedorov <fedorgle@fit.cvut.cz>
2021-06-26 20:26:39 +0300

    Added a base for VkBot and 2 helper functions for working with vk Responses

---
+++ src/Vk/VkBot.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE TypeFamilies #-}
+module Vk.VkBot where
+
+import Vk.Types as VT
+import Bot
+
+
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Data.Maybe
+import qualified Data.Text as T
+import Data.Functor ((<&>))
+import Control.Monad (liftM2)
+
+-- | VkBot is a data type used as an instance of the Bot class to define interactions with the Vk.
+--  Contains baseUrl, token used for authentication and an id of a Vk group, the bot is attached to. 
+data VkBot
+    = VkBot
+    { baseUrl :: String
+    , token :: String
+    , clubId :: String
+    }
+-- | VkMessage is DTO used to send messages via Vk
+data VkMessage
+    = VkMessage
+    { user_id :: Int -- ^ Id of the receoient
+    , random_id :: Int -- ^ A quirk of the vk API. It requires, that all messages have a ranom number attached to them
+    , message :: String -- ^ the message itself
+    }
+
+-- | Instance of Bot for Telegram
+--   Unlike telegarm, Vk uses two Apis for chat bots. 
+--   The main one is used to get accses to the LongPolling server and SendMessages.
+--   LongPolling api is accessed via separate url and token, returned from the Main API.
+--   All upadtes are fetched from the LongPollingAPI
+--   `type Destination VkBot = Int` Destination is just an Id of a message recepient
+--   `type Session VkBot = ServerKeyTs` Session contains a url of LongPolling server, temporary token and id of the latest message. All of this is provided by Vk after fetching the Long Poll server from the main API.
+instance Bot VkBot where
+    type Destination VkBot = Int 
+    type Session VkBot = ServerKeyTs
+    -- | initialSession when we do not have the long polling server yet
+    initialSession _ = ServerKeyTs  "" "" "0" 
+    -- | getUpdates aquires the long polling server info, if it is not yet present and then uses it get Updates about the new messages from the Vk.
+    getUpdates bot manager session@(ServerKeyTs server key ts) = undefined 
+
+    -- | sendMessage simply sends a message through Vk using VkMessage DTO to a provided destination.
+    sendMessage bot manager dest message = undefined 
+
+-- | extractTextAndSender gets user_id and message text from an update if present
+extractTextAndSender :: VT.Update -> Maybe (Int, L8.ByteString)
+extractTextAndSender u = liftM2 (,) s t
+    where t = (oMessage (uObject u) <&> mText) <&> L8.pack . T.unpack
+          s = oMessage (uObject u) <&> mFromId
+
+-- | newUpdates appliese extractTextAndSender to all updates in a Response
+newUpdates :: Maybe VT.Response ->  Maybe [(Int, L8.ByteString)]
+newUpdates r = r >>= rUpdates >>= traverse  extractTextAndSender
