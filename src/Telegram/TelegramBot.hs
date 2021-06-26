8aa6084d22d25425974f9b75dbf6a7d21a7c0f79
Gleb Fedorov <fedorgle@fit.cvut.cz>
2021-06-26 20:00:50 +0300

    Added getUpdates implementation for the TelegramBot

--- src/Telegram/TelegramBot.hs
+++ src/Telegram/TelegramBot.hs
@@ -3,13 +3,19 @@
 module Telegram.TelegramBot where
 
 import Bot ( Bot(..) )
+import Config
 import Telegram.Types as TT
 import GHC.Generics ( Generic )
 import Data.Aeson
+import Network.HTTP.Client
 import qualified Data.ByteString.Lazy.Char8 as L8
-
-
-
+import Data.Functor ((<&>))
+import Control.Monad (liftM2, guard)
+import qualified Data.Text as T
+import Control.Applicative ((<|>))
+import Data.Traversable
+import Data.Maybe (fromMaybe)
+import qualified Data.Bifunctor
 
 
 -- | Data type used as an instance of the Bot class to define interactions with the Telegram.
@@ -39,8 +45,56 @@ instance Bot TelegramBot where
     
     -- | getUpdates receives new messages form the Telegram and extracts sender and message from them.
     --   If session is equal to the Initial session, getUpdates makes request to get Id of the last message
-    getUpdates bot manager session = undefined 
-
+    getUpdates bot manager session = do
+        s <- if session == initialSession bot then
+            getSession bot manager
+            else return session
+        let req = parseRequest_ $ fullUrl bot <> "/getUpdates?offset=" <> show session <> "&timeout=25"
+        response <-  httpLbs req manager -- simpleGet manager $ fullUrl bot <> "/getUpdates?offset=" <> show offset 
+        let tgResponse = (decode (responseBody response) :: Maybe TT.Response)
+        let mUpdates = newUpdates tgResponse
+        let newSession = updateSession session tgResponse
+        return $ case mUpdates of
+            Just updates -> Right (newSession, map (Data.Bifunctor.second (L8.pack . T.unpack)) updates)
+            _            -> if newSession == session
+                                then Left "Failed to fetch updates"
+                                else Right (newSession, []) 
     -- | sendMesasge sends a POST request to the telegram to send a message as a response to a user.
+    
     sendMessage bot manager dest message = undefined 
- 
+     
+getSession :: TelegramBot -> Manager -> IO (Session TelegramBot)
+-- ^ getSession makes the same request as the getUpdates funtion, but with a negative offset, so we can learn what is the latest message, that was sent to us
+--   so we can read messages starting from the next one. (Haddock couldn't parse commets above this function for some reason)
+getSession b m = do
+    let req = parseRequest_ $ fullUrl b <> "/getUpdates?offset=-1&timeout=25"
+    response <-  httpLbs req m
+    let tgResponse = (decode (responseBody response) :: Maybe TT.Response)
+    return $ updateSession (initialSession b) tgResponse
+
+-- | getTextAndChatId simply extracts two necessery fields from the Telegram update, if they are present
+getTextAndChatId :: TT.Update -> Maybe (Int, T.Text)
+getTextAndChatId u = liftM2 (,) chatId messageText
+    where message = rMessage u <|> rEditedMessage u
+          chatId = message <&> mChat <&> cId
+          messageText = message >>= mText
+
+-- | newUpdates applies getTextAndChatId to all updates in a Response
+newUpdates ::  Maybe TT.Response -> Maybe [(Int, T.Text)]
+newUpdates r = (r >>= rResult) >>= traverse getTextAndChatId 
+
+-- | updateSession goes through the updates and finds the latest one, so we can read updates from there on. In case of an error session stays the same.
+updateSession :: Session TelegramBot -> Maybe TT.Response -> Session TelegramBot
+updateSession oldSession r =
+    fromMaybe oldSession $ (nonEmptyOrNothing mUpdates <&> map rUpdateId <&> maximum) <&> (+ 1)
+    where mUpdates = r >>= rResult
+          nonEmptyOrNothing Nothing = Nothing
+          nonEmptyOrNothing (Just []) = Nothing
+          nonEmptyOrNothing x = x
+
+fullUrl :: TelegramBot -> String
+fullUrl bot = baseUrl <> tbToken bot
+
+createBot :: Config -> TelegramBot
+createBot (Config (TelegramConfig token) _) = TelegramBot token
+createBot _ = error "Invalid config type."
